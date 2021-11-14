af95ebf4e985563cf6b9027950bc23a07eb020cf
mongchanghsi <m.changhsi@gmail.com>
2021-11-14 15:18:16 +0800

    Removed redundant import

--- src/main/java/com/ericmonghackathonlogisticsswag/HackathonLogisticsSWAG/repository/RedemptionRepository.java
+++ src/main/java/com/ericmonghackathonlogisticsswag/HackathonLogisticsSWAG/repository/RedemptionRepository.java
@@ -4,8 +4,6 @@ import com.ericmonghackathonlogisticsswag.HackathonLogisticsSWAG.entity.Redempti
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
 public interface RedemptionRepository extends CrudRepository<Redemption, Long> {
 }
